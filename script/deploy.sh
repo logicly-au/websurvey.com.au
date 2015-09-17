be0009fe38a28702ec9849c60a430e8bfc971abf
Adam Clarke <adam.clarke@strategicdata.com.au>
2015-09-17 11:46:26 +1000

    look for deploy key in right place.

--- script/deploy.sh
+++ script/deploy.sh
@@ -2,7 +2,7 @@
 set -e # halt script on error
 
 openssl aes-256-cbc -K $encrypted_a842c56f2ce8_key -iv $encrypted_a842c56f2ce8_iv \
-  -in deploy_key.travis -out deploy_key -d
+  -in .travis/deploy_key.travis -out .travis/deploy_key -d
 
 eval "$(ssh-agent -s)" #start the ssh agent
 chmod 600 .travis/deploy_key
