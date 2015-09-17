b3834a94d0178cb88cb3f3e059247790cf3c0b19
Adam Clarke <adam.clarke@strategicdata.com.au>
2015-09-17 12:11:04 +1000

    Revert to correct method for deployment.

--- script/deploy.sh
+++ script/deploy.sh
@@ -7,7 +7,5 @@ openssl aes-256-cbc -K $encrypted_a842c56f2ce8_key -iv $encrypted_a842c56f2ce8_i
 eval "$(ssh-agent -s)" #start the ssh agent
 chmod 600 .travis/deploy_key
 ssh-add .travis/deploy_key
-git fetch origin
-git checkout -b gh-pages origin/gh-pages
-git merge master
-git push
+git remote add gh-pages git@github.com:strategicdata/websurvey.com.au.git
+git push gh-pages
