035d3854792cb520beaf982b48f3d9023917430e
Adam Clarke <adam.clarke@strategicdata.com.au>
2015-09-17 12:14:17 +1000

    Testing deploy 1.

--- script/deploy.sh
+++ script/deploy.sh
@@ -7,5 +7,6 @@ openssl aes-256-cbc -K $encrypted_a842c56f2ce8_key -iv $encrypted_a842c56f2ce8_i
 eval "$(ssh-agent -s)" #start the ssh agent
 chmod 600 .travis/deploy_key
 ssh-add .travis/deploy_key
+git config --global push.default simple
 git remote add gh-pages git@github.com:strategicdata/websurvey.com.au.git
 git push gh-pages
